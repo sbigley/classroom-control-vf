594729c6a12227493f78f86f781a16490f6b5a5f
sbigley <steve@bigley.com>
2016-10-13 15:46:11 -0400

    update nginx/manifests/init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,27 +2,24 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
+
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
   
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
@@ -30,16 +27,10 @@ class nginx {
   
   file { '/etc/nginx/conf.d':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
   
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
