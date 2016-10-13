68032b07a1eeb0ccb4e6488bd027906af4a8b586
sbigley <steve@bigley.com>
2016-10-13 09:53:35 -0400

    hosts aliases update

--- manifests/site.pp
+++ manifests/site.pp
@@ -61,7 +61,7 @@ node default {
 
   host { 'testing.puppetlabs.vm':
     ensure => present,
-    host_aliases => ['localhost', 'localhost.localdomain', 'testing'],
+    host_aliases => ['localhost', 'localhost.localdomain', 'testing', 'steve'],
     ip => '127.0.0.1',
   }
 
